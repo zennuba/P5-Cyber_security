419cdfd0ed927ec6a1ee4fb00176b6c1ba279c5f
Lotte Nielsen <lotnie01@iba.dk>
2026-05-05 14:13:28 +0200

    første commit

---
+++ js/script-screens.js
@@ -0,0 +1,41 @@
+const screens = {
+    intro: {
+        type: "intro",
+        title: "Cyber Hero",
+        text: "Træn dine kompetencer i at spotte cyber trusler",
+        button: "Start",
+        next: "dilemma-1"
+        },
+/*
+    "dilemma-1": 
+    {
+        type: "dilemma-1",
+        title: "Du modtager en SMS",
+        text: "SMS’en siger, at din eksamensdato er ændret. Du skal klikke på linket for at booke en ny dato. Hvad gør du?",
+        choices: 
+        [      
+        {
+            text: "Jeg klikker på linket med det samme. Jeg vil ikke risikere at misse min eksamen.",
+            next: "consequence-1a"
+        },
+        {
+            text: "Jeg logger ind på skoles officielle platform og tjekker mine eksamensdatoer uden at klikke på linket i SMS'en.",
+            next: "consequence-1b"
+        },
+        {
+            text: "Jeg sletter SMS'en og ignorerer den. Jeg antager at det bare er spam."
+        }
+        ]      
+    ,}
+
+    consequence-1a: {
+        type: "consequence",
+        title: "Risiko",
+        text: Du bliver sen
+        button: " ",
+        next: "review-1a"
+        }
+    },
+    
+
+*/
